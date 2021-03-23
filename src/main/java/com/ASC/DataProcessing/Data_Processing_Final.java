d8d5e9881dd8a6497737611a2eb90ed93efaaafb
Administrator <bhavyajoshi1510@gmail.com>
2021-03-23 20:54:47 +0000

    ArrayList and map code is replaced by Json Object creation directly. (Not completed yet)

---
+++ src/main/java/com/ASC/DataProcessing/Data_Processing_Final.java
@@ -0,0 +1,130 @@
+package com.ASC.DataProcessing;
+
+import org.apache.commons.lang3.ArrayUtils;
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+public class Data_Processing_Final {
+
+    public static final String headerTagPath ="//*[@id=\"DocList1_ContentContainer1\"]/table/tbody/tr[1]/td/div/div[1]/table/thead/tr";
+    public static final String mainTablePath = "//*[@id=\"DocList1_ContentContainer1\"]/table/tbody/tr[1]/td/div/div[2]/table";
+    public static final String nextButtonPath = "//*[@id=\"DocList1_LinkButtonNext\"]";
+    public String[] grabHeader(WebDriver driver)
+    {
+        WebElement headerTag =  driver.findElement(By.xpath(headerTagPath));
+        List<WebElement> headers = headerTag.findElements(By.tagName("th"));
+        String[] header = new String[headers.size()];
+        for (int i =0;i<headers.size();i++)
+        {
+            header[i]=headers.get(i).getText();
+        }
+        header = ArrayUtils.remove(header,0);
+        header = modifyHeader(header);
+        return header;
+    }
+
+    private String[] modifyHeader(String[] hdr)
+    {
+        String header = Arrays.toString(hdr);
+        header = header.replace("Type Desc.","Type_Desc");
+        header = header.replace("Type","Type_c").replace("Name/ Corporation","Name_Corporation_c");
+        header = header.replace("Book","Book_c").replace("Page","Page_c").replace("Type_c_Desc","Type_Desc_c");
+        header = header.replace("Rec. Date","Rec_Date_c").replace("Street #","Street_c");
+        header = header.replace("Property Descr","Property_Descr_c").replace("Town","Town_c");
+        header = header.replace("[","").replace("]","");
+        hdr = header.split(",");
+        return hdr;
+    }
+
+    public void tableData(WebDriver driver,String fileName)
+    {
+        String[] headers = grabHeader(driver);
+        JSONArray tableDataContent;
+        tableDataContent = grabData(driver,headers);
+
+        boolean checkNext = true;
+
+        while(checkNext){
+            try{
+                WebElement nextBtn = driver.findElement(By.xpath(nextButtonPath));
+                nextBtn.click();
+                Thread.sleep(5000);
+                tableDataContent = appendToList(tableDataContent,grabData(driver,headers));
+            }
+            catch (Exception e1){
+                checkNext = false;
+            }
+        }
+
+        JSONObject jsonObj = generateJson(tableDataContent);
+
+        try {
+            File myObj = new File("C:\\JsonResponse\\"+fileName+".txt");
+            if(myObj.createNewFile()) {
+                FileWriter myWriter = new FileWriter("C:\\JsonResponse\\"+fileName+".txt");
+                myWriter.write(jsonObj.toString());
+                myWriter.close();
+            }
+        } catch (IOException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+    }
+
+    public JSONArray grabData(WebDriver driver,String[] header)
+    {
+        JSONArray objForPage = new JSONArray();
+        JSONObject objForRow = new JSONObject();
+
+        JSONObject staticData = new JSONObject();
+        staticData.put("type", "Lead_Search_c");
+        staticData.put("referenceId","ref");
+
+        WebElement table =  driver.findElement(By.xpath(mainTablePath));
+       int rowSize = table.findElements(By.tagName("tr")).size();
+
+        for (int rowCount=1;rowCount<=rowSize;rowCount++)
+        {
+            WebElement row = driver.findElement(By.xpath(getMainTableRow(rowCount)));
+
+            List<WebElement> cols = row.findElements(By.tagName("td"));
+            for (int column = 0, hdr = 0; (column < cols.size()); column++, hdr++) {
+                if (column != 0) {
+                    objForRow.put(header[hdr - 1], cols.get(column).getText());
+                }
+            }
+            //objForRow.put("attributes",staticData.append("referenceId","ref"+rowCount));
+            objForRow.put("attributes",staticData);
+            objForRow.put("Grantor_c",new GrantorData().getGrantorData(driver,rowCount));
+            objForPage.put(objForRow);
+            objForRow = new JSONObject();
+        }
+        return objForPage;
+    }
+
+    public JSONObject generateJson(JSONArray jsonArray)
+    {
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("records", jsonArray);
+        return jsonObject;
+    }
+
+    public JSONArray appendToList(JSONArray original,JSONArray toBeAppend)
+    {
+        return original.put(toBeAppend);
+    }
+
+    public String getMainTableRow(int count){
+        return "//*[@id=\"DocList1_ContentContainer1\"]/table/tbody/tr[1]/td/div/div[2]/table/tbody/tr["+count+"]";
+    }
+}
---
+++ src/main/java/com/ASC/DataProcessing/GrantorData.java
@@ -0,0 +1,110 @@
+package com.ASC.DataProcessing;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GrantorData {
+
+    public final String[] headerSubTable = {"Name", "Type_c"};
+
+    public static final String labelPath = "//*[@id=\"DocDetails1_Label_GrantorGrantee\"]";
+
+    public JSONArray getGrantorData(WebDriver driver, int rowID) {
+
+        WebDriverWait wait=new WebDriverWait(driver, 20);
+
+        WebElement linkedText;
+        linkedText = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(getButtonXpath(rowID))));
+        linkedText.click();
+        String grantorLabel = driver.findElement(By.xpath(labelPath)).getText();
+        String[] number = grantorLabel.split("Grantor/Grantee-");
+
+        int dataRecords = Integer.parseInt(number[1]);
+
+        if (dataRecords<=10){
+
+            return getActualGrantorData(listOfRows(driver,dataRecords));
+
+        }else{
+         return getMultipleGrantorData(driver,dataRecords);
+        }
+    }
+
+    private List<WebElement> listOfRows(WebDriver driver,int length)
+    {
+        List<WebElement> rowOfSubTable = new ArrayList<>();
+        for (int itr=2;itr<=length+1;itr++){
+            rowOfSubTable.add(driver.findElement(By.xpath("//*[@id=\"DocDetails1_GridView_GrantorGrantee\"]/tbody/tr["+itr+"]")));
+        }
+        return rowOfSubTable;
+    }
+    private JSONArray getMultipleGrantorData(WebDriver driver,int dataRecords){
+        int noOfPages = dataRecords/10;
+        int dataInLastPage = dataRecords % 10;
+
+        JSONArray subTableContent = getActualGrantorData(listOfRows(driver,10));
+        int pageCount = 1;
+
+        if (pageCount<noOfPages){
+            try{
+                WebElement nextBtn = driver.findElement(By.xpath("//*[@id=\"DocDetails1_GridView_GrantorGrantee\"]/tbody/tr[12]/td/table/tbody/tr/td["+(pageCount+1)+"]/a"));
+                nextBtn.click();
+                Thread.sleep(5000);
+                subTableContent = appendToList(subTableContent,getActualGrantorData(listOfRows(driver,10)));
+                pageCount++;
+            }
+            catch (Exception e1){
+                e1.printStackTrace();
+            }
+        }else {
+            try {
+                WebElement nextBtn = driver.findElement(By.xpath("//*[@id=\"DocDetails1_GridView_GrantorGrantee\"]/tbody/tr[12]/td/table/tbody/tr/td[" + (pageCount + 1) + "]/a"));
+                nextBtn.click();
+                Thread.sleep(5000);
+                subTableContent = appendToList(subTableContent, getActualGrantorData(listOfRows(driver, dataInLastPage + 2)));
+            }catch(Exception e2){}
+        }
+        return subTableContent;
+    }
+
+    private JSONArray appendToList(JSONArray original,JSONArray toBeAppend)
+    {
+        return original.put(toBeAppend);
+    }
+    private JSONArray getActualGrantorData(List<WebElement>  rowElement){
+
+        JSONArray objForSubTable = new JSONArray();
+        JSONObject objForSubRow = new JSONObject();
+
+        for (WebElement r:rowElement) {
+            List<WebElement> cols = r.findElements(By.tagName("td"));
+            for (int i = 0; i < cols.size(); i++) {
+                objForSubRow.put(headerSubTable[i],cols.get(i).getText());
+            }
+            objForSubTable.put(objForSubRow);
+            objForSubRow = new JSONObject();
+        }
+       return objForSubTable;
+    }
+
+    private String getButtonXpath(int rowValue){
+        if(rowValue<9){
+            return "//*[@id=\"DocList1_GridView_Document_ctl0"+(rowValue+1)+"_ButtonRow_Name/ Corporation_"+(rowValue-1)+"\"]";
+        }else{
+            return "//*[@id=\"DocList1_GridView_Document_ctl"+(rowValue+1)+"_ButtonRow_Name/ Corporation_"+(rowValue-1)+"\"]";
+        }
+    }
+
+   /* public JSONArray appendToList(JSONArray original,JSONArray toBeAppend)
+    {
+        return original.put(toBeAppend);
+    }*/
+}
