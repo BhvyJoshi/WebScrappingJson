6f97b39e2b13cad86cfef413358919902fe7ebf5
Administrator <bhavyajoshi1510@gmail.com>
2021-04-16 11:28:14 +0000

    southEssex county header parsing is done

--- src/main/java/com/ASC/Common/SouthEssexHelperClass.java
+++ src/main/java/com/ASC/Common/SouthEssexHelperClass.java
@@ -46,6 +46,7 @@ public class SouthEssexHelperClass {
                 header[i] = header[i].trim();
         }
         header = Arrays.stream(header).distinct().toArray(String[]::new);
+        header = ArrayUtils.removeAll(header,0,1,2);
         return modifyHeader(header);
     }
 
@@ -53,13 +54,14 @@ public class SouthEssexHelperClass {
     {
         String header = Arrays.toString(hdr);
         header = header.replace(", ",",");
-        header = header.replace("DATE","DATE__c");
-        header = header.replace("Type","Type__c").replace("FIRST PARTY NAME","FIRST_PARTY_NAME__c").replace("ROLE","ROLE__c");
-        header = header.replace("Book","Book__c").replace("Page","Page__c").replace("Type","Type__c").replace("SECOND PARTY NAME","SECOND_PARTY_NAME__c");
-        header = header.replace("DESC","DESC__c").replace("Street","Street__c").replace("Town","Town__c");
-        header = header.replace("Locus","Locus__c").replace("PBK","PBK__c").replace("PPG","PPG__c").replace("Consideration","Consideration__c");
+        header = header.replace("DATE","DATE__c").replace("ROLE","ROLE__c");
+        header = header.replace("Type","Type__c").replace("FIRST PARTY NAME","FIRST_PARTY_NAME__c");
+        header = header.replace("Book","Book__c").replace("Page","Page__c");
+        header = header.replace("DESC","DESC__c").replace("Street","Street__c");
+        header = header.replace("Locus","Locus__c").replace("PBK","PBK__c");
+        header = header.replace("PPG","PPG__c").replace("Consideration","Consideration__c");
+        header = header.replace("SECOND PARTY NAME","SECOND_PARTY_NAME__c").replace("Town","Town__c");
         header = header.replace("[","").replace("]","");
-        hdr = header.split(",");
-        return hdr;
+        return header.split(",");
     }
 }
