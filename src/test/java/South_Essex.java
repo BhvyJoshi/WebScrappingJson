4489e022096b881d23ec24296f59cb9fe9461a27
Administrator <bhavyajoshi1510@gmail.com>
2021-04-22 19:34:23 +0000

    Done with Group3, North_Bristol, South_Essex,Hampden classs testing, working completely fine and updated testng.xml file accordingly

--- src/test/java/South_Essex.java
+++ src/test/java/South_Essex.java
@@ -3,24 +3,24 @@ import com.ASC.Common.InitializerClass;
 import com.ASC.Common.SouthEssexHelperClass;
 import org.openqa.selenium.WebDriver;
 import org.testng.annotations.AfterTest;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
-//Completey Done
+//completed.
+
 public class South_Essex extends SouthEssexHelperClass {
     public WebDriver driver;
-    public static String url = "https://www.masslandrecords.com";
+   /* public static String url = "https://www.masslandrecords.com";
     public static String value= "South Essex";
     public static String keyWord = "lender";
     public static String fileName = "demo_"+value;
-    //public static String firstName = "abc";
-
-
+    public static String firstName = "abc";*/
     @Test
-    public void test(){
+    @Parameters({"url","value","keyWord","firstName","fileName","request"})
+    public void test(String url,String value,String keyWord, String firstName,String fileName,String request){
         driver = InitializerClass.initialize(url,value);
-        firstPage(driver,keyWord);
-        //grabHeader(driver);
-        tableData(driver,fileName,"123456");
+        firstPage(driver,keyWord,firstName);
+        tableData(driver,fileName,request);
     }
 
     @AfterTest
